9886ec3ca4cc8dff098734164fd4661a1047a03e
Sintu kumar <Sintu kumar@DESKTOP-54ML9PE>
2024-11-12 02:04:43 +0530

    commit

--- GitProject01/src/main/java/com/nt/main/App.java
+++ GitProject01/src/main/java/com/nt/main/App.java
@@ -6,7 +6,7 @@ public class App {
 	public  int sum(int n1,int n2)
 	{
 		return n1+n2;
-		
+	
 	}
     public static void main(String[] args) {
 
@@ -16,6 +16,6 @@ public class App {
       System.out.println("Sum of both number in dev-1 : "+sum);
 
       System.out.println("Sum of both number in TL : "+sum);
-
+      
     }
 }
