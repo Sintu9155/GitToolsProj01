6a4e5c4fa156ab28bff53d6e694a5e25e1a41f6e
Sintu kumar <Sintu kumar@192.168.1.6>
2024-11-26 00:42:09 +0530

    push the jenkins file

---
+++ JenkinsProj01/src/main/java/com/nt/servlet/DateTimeServlet.java
@@ -0,0 +1,33 @@
+package com.nt.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/dateurl")
+public class DateTimeServlet extends HttpServlet{
+      
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter writer = resp.getWriter();
+		
+		Date d=new Date();
+		writer.println("<h1> Date And Time is ::"+d+"</h1>");
+		
+		writer.println("<br><a href='index.jsp'>Home</a>");
+		writer.close();
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+}
---
+++ JenkinsProj01/src/main/webapp/index.jsp
@@ -0,0 +1,2 @@
+<h1 style="color:red;text-align:center">Welcome Jenkins Date And Time</h1>
+<a href="dateurl">Show Date And Time</a>
