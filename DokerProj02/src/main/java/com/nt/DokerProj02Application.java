4808034f1fa433ddf3a09a5c98b26bdab9e0a7c4
Sintu kumar <Sintu kumar@host.docker.internal>
2024-11-29 19:20:44 +0530

    1st push

---
+++ DokerProj02/src/main/java/com/nt/DokerProj02Application.java
@@ -0,0 +1,14 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DokerProj02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DokerProj02Application.class, args);
+		System.out.println("Welcome to Docker Spring Boot App");
+	}
+
+}
